5fb539a65c75addcc77c0ab84082d11d9cbfcf74
Unknown <mahieu66@gmail.com>
2018-02-02 13:50:04 +0100

    oublie
    oublie de transformer else { if() {} en else if() {}

--- main.js
+++ main.js
@@ -12,10 +12,8 @@ bot.on('message', function(message) {
   console.log(message);
   if (message.content.startsWith(config.prefix + "ping")) {
     modules.ping(message);
-  } else {
-    if (message.content.startsWith(config.prefix + "lance")) {
-      modules.alea(message, config);
-    }
+  } else if (message.content.startsWith(config.prefix + "lance")) {
+    modules.alea(message, config);
   }
 })
 
